08b0935f4486ee2d37ee9e862b75926f81598460
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-19 23:44:50 +0300

    Added login form markup and style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/**
+ * @hints
+ * state:  const [value, setValue] = useState(initialValue);
+ * input: value, onChange
+ * form, input: onSubmit
+ * @end
+ */
+/**
+ * - Handle input changes
+ * - Check user credentials
+ * - On failed login
+ * -- Show error message
+ * - On successful login
+ * -- Clear inputs
+ * -- Clear error
+ * -- Alert user login
+ */
+function LoginForm() {
+  return (
+    <form>
+      <div className="error">Incorrect username or password</div>
+      <input name="email" type="email" required placeholder="Email" />
+      <input name="password" type="password" required placeholder="Password" />
+      <input type="submit" value="Login" />
+    </form>
+  );
+}
+
+ReactDOM.render(<LoginForm />, document.getElementById("root"));
