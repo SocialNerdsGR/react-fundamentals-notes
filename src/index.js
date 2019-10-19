6c8d5dfa20a77f565c1b6c4b6784de00c33a9a73
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-19 18:57:43 +0300

    Added event handling notes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/**
+ * - camelCase
+ * - preventDefault
+ * - Syntethic events
+ * - Binding
+ * - Parameters
+ * - Function components
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <a href="#">Link</a>
+        <button>Click</button>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
