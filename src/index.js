4d1cec38a15074e6b474e9d29030029d141fe16d
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-19 19:15:19 +0300

    Added conditional rendering notes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/**
+ * - Components
+ * - Using variables
+ * - && operator
+ * - Inline conditional operator
+ * - Attrtibutes
+ * - Prevent component to render
+ */
+function App() {
+  return <div></div>;
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
