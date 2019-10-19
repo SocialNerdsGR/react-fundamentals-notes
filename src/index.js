cb4c1a5c077c21cd01bf97e882039fc6fdb7acc5
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-19 20:34:02 +0300

    Added controlled form notes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/**
+ * - Difference from other DOM elements
+ * - Controlled
+ * - Input
+ * - Checkox
+ * - Select
+ * - Multiple inputs
+ * - Read-only
+ */
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" value={} onChange={} />
+        <select>
+          <option>Java</option>
+          <option>JavaScript</option>
+          <option>Python</option>
+        </select>
+        <input type="checkbox" />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<Form />, document.getElementById("root"));
