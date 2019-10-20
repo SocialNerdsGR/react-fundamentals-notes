9de9e64e26f51a3684bceb0cdb05c0b6f72cf2bd
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-20 09:47:06 +0300

    Added http notes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/**
+ * - Use axios to load HN data
+ * - https://hn.algolia.com/api/v1/search?query=react
+ * - Render a list with the results
+ * - Use only the title
+ */
+class HackerNews extends React.Component {}
+
+ReactDOM.render(<HackerNews />, document.getElementById("root"));
