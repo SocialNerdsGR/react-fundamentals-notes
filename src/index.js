b5ea421da665ee9b012dfabf3d0c639c1094df5c
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-24 00:19:35 +0300

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-/**
- * - Components
- * - Using variables
- * - && operator
- * - Inline conditional operator
- * - Attrtibutes
- * - Prevent component to render
- */
 function App() {
   return <div></div>;
 }
