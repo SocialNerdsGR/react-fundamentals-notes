fea5123c3d3afabf2e15dfedabcea66b7fe03376
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-27 23:34:21 +0200

    Added lists example boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class List extends React.Component{
+const startWarsMovies = [
+  {
+    "title": "Star Wars: Episode I - The Phantom Menace",
+    "episode_number": "1"
+  },
+  {
+    "title": "Star Wars: Episode II - Attack of the Clones",
+    "episode_number": "2"
+  },
+  {
+    "title": "Star Wars: Episode III - Revenge of the Sith",
+    "episode_number": "3"
+  },
+  {
+    "title": "Star Wars: Episode IV - A New Hope",
+    "episode_number": "4"
+  },
+  {
+    "title": "Star Wars: Episode V - The Empire Strikes Back",
+    "episode_number": "5"
+  },
+  {
+    "title": "Star Wars: Episode VI - Return of the Jedi",
+    "episode_number": "6"
+  }
+];
+
+class List extends React.Component {
+  state = {
+    movies: startWarsMovies
+  };
+
+  /**
+   *  Array.map((item) => (/return element here/));
+   *  key={}
+   */
   render() {
     return <ul></ul>;
   }
