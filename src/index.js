4f398ff97fb47e92c05f36c2d9a9803b83f3016d
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-27 23:59:17 +0200

    Added conditional rendering boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component{
+class App extends React.Component {
+  state = {
+    logged: false
+  };
+
   render() {
     return <div>Welcome Guest!</div>;
   }
