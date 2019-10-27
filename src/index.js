7bd9f540a231341a392a7dbb8d9618740f5bad13
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-27 23:21:58 +0200

    Updated lifecycled methods example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-/**
- * - What is lifecycle methods
- * - Register events
- * - Clear events
- */
 class Lifecycle extends React.Component {
   render() {
     return <h1>Hello world!</h1>;
