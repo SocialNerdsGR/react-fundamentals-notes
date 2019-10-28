58f107e9f527f659bddcc7fbc1ef1be0310d22a7
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-28 10:16:40 +0200

    Added decrease quantity exercise boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,30 @@ export default class App extends React.Component {
     const { cartItems } = this.state;
     const item = cartItems.find(item => item.id === product.id);
     if (item) {
-      return;
+      return this.increaseQuantity(product);
     }
 
-    this.setState({cartItems: [product, ...cartItems]});
+    this.setState({cartItems: [{...product, quantity: 1}, ...cartItems]});
+  };
+
+  removeFromCart = (id) => {
+    const { cartItems } = this.state;
+    const filteredItems = cartItems.filter(item => item.id !== id);
+    this.setState({cartItems: filteredItems});
+  };
+
+  increaseQuantity = (product) => {
+    const {cartItems} = this.state;
+
+    const items = cartItems.map(item => {
+      if (item.id === product.id) {
+        return {...item, quantity: item.quantity + 1};
+      }
+
+      return item;
+    });
+
+    this.setState({cartItems: [...items]});
   };
 
   render() {
@@ -50,7 +70,10 @@ export default class App extends React.Component {
               {
                 cartItems.map(item => (
                   <li key={item.id}>
+                    <span>{item.quantity}-</span>
                     <span>{item.name}</span>
+                    <button onClick={() => this.increaseQuantity(item)}>+</button>
+                    <button className={`cart-remove`} onClick={() => this.removeFromCart(item.id)}>x</button>
                   </li>
                 ))
               }
