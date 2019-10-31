755bd2fbb3c0dbbc5005e31fdadcd65f5289289c
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-31 23:55:42 +0200

    Added login form solution

--- src/index.js
+++ src/index.js
@@ -2,34 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-/**
- * @hints
- * state:  const [value, setValue] = useState(initialValue);
- * input: value, onChange
- * form, input: onSubmit
- * @end
- */
-/**
- * - Handle input changes
- * - Check user credentials
- * - On failed login
- * -- Show error message
- * - On successful login
- * -- Clear inputs
- * -- Clear error
- * -- Alert user login
- */
 class LoginForm extends React.Component {
+  state = {
+    error: '',
+    email: '',
+    password: ''
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {email, password} = this.state;
+    if (email !== 'thanos@socialnerds.gr' || password !== '123456') {
+      this.setState({error: 'Incorrect username or password'});
+      return;
+    }
+
+    this.setState({
+      email: '',
+      password: '',
+      error: ''
+    });
+  };
+
+  handleFields = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  };
+
   render() {
+    const {email, password, error} = this.state;
+
     return (
-      <form>
-        <div className="error">Incorrect username or password</div>
-        <input name="email" type="email" required placeholder="Email" />
-        <input name="password" type="password" required placeholder="Password" />
-        <input type="submit" value="Login" />
+      <form onSubmit={this.handleSubmit}>
+        <div className="error">{error}</div>
+        <input
+          name="email"
+          type="email"
+          required
+          placeholder="Email"
+          value={email}
+          onChange={this.handleFields}
+        />
+        <input
+          name="password"
+          type="password"
+          required
+          placeholder="Password"
+          value={password}
+          onChange={this.handleFields}
+        />
+        <input type="submit" value="Login" onChange={this.handleFields}/>
       </form>
     );
   }
 }
 
-ReactDOM.render(<LoginForm />, document.getElementById("root"));
+ReactDOM.render(<LoginForm/>, document.getElementById("root"));
