29b4291d9367bb5ab8525919e5fe0c8ea794cb31
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-31 23:43:12 +0200

    Updated forms example

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
  * - Difference from other DOM elements
  * - Controlled
  * - Input
- * - Checkox
+ * - Checkbox
  * - Select
  * - Multiple inputs
  * - Read-only
@@ -14,7 +14,7 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <input type="text" value={} onChange={} />
+        <input type="text" />
         <select>
           <option>Java</option>
           <option>JavaScript</option>
