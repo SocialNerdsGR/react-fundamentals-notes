f2aebe1d6df1d88265acc7b05a47a1db2d3362a4
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-03 19:21:29 +0200

    Updated JSX example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-/**
- * JSX
- * Expressions
- * Attributes
- * Closing syntax
- * createElement
- */
-
 ReactDOM.render(<h1>Hello world</h1>, document.querySelector("#root"));
