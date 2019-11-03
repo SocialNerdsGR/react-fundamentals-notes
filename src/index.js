339c62cd7e2703daa4aae11f767c2a091534f125
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-04 00:55:56 +0200

    Added HTTP solution

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios';
 
-/**
- * - axios.get('https://hn.algolia.com/api/v1/search?query=react')
- */
 class HackerNews extends React.Component {
   state = {
     news: []
   };
 
   async componentDidMount() {
-    // Implement HTTP request here.
+    const response = await axios.get('https://hn.algolia.com/api/v1/search?query=react');
+    this.setState({news: response.data.hits});
   }
 
   render() {
     return <ul>
       {
         this.state.news.map((item => {
-          return <li>
-          {/*  Render each item here  */}
+          return <li key={item.objectID}>
+            {item.title}
           </li>
         }))
       }
@@ -26,4 +25,4 @@ class HackerNews extends React.Component {
   }
 }
 
-ReactDOM.render(<HackerNews />, document.getElementById("root"));
+ReactDOM.render(<HackerNews/>, document.getElementById("root"));
