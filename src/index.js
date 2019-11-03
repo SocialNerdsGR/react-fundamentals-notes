0628a497afa369607d01601c5bdf39f71187e434
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-03 23:21:53 +0200

    Added the completed form example

--- src/index.js
+++ src/index.js
@@ -2,16 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Form extends React.Component {
+  state = {
+    text: '',
+    checkbox: false,
+    select: 'Java'
+  };
+
+  checkboxHandler = (event) => {
+    this.setState({checkbox: event.target.checked});
+  };
+
+  selectHandler = (event) => {
+    this.setState({select: event.target.value});
+  };
+
+  textHandler = (event) => {
+    this.setState({text: event.target.value});
+  };
+
+  fieldsHandler = (event) => {
+    const value = event.target.type !== 'checkbox' ? event.target.value : event.target.checked;
+    this.setState({[event.target.name]: value});
+  };
+
+  formHandler = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
+    const {text, checkbox, select} = this.state;
     return (
-      <form>
-        <input type="text" />
-        <select>
+      <form onSubmit={this.formHandler}>
+        <input type="text" value={text} name="text" onChange={this.fieldsHandler} />
+        <select value={select} name="select" onChange={this.fieldsHandler}>
           <option>Java</option>
           <option>JavaScript</option>
           <option>Python</option>
         </select>
-        <input type="checkbox" />
+        <input type="checkbox" value={checkbox} name="checkbox" onChange={this.fieldsHandler} />
         <input type="submit" />
       </form>
     );
