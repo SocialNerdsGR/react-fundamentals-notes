cd4741d1935309a39b43aee6a7c3ccde0889e69a
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-05 20:14:18 +0200

    Updated events handling notes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-/**
- * - camelCase
- * - preventDefault
- * - Syntethic events
- * - Binding
- * - Parameters
- * - Disable handler
- * - Function components
- */
 class App extends React.Component {
   render() {
     return (
