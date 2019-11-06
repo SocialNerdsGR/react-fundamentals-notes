acb07a0151f098bb76d7cbc776891f83a85e518a
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-06 20:53:51 +0200

    Updated notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-export default function App() {
-  return (
-    <div className="app">
-      <h1>SocialNerds</h1>
-      <input className="search" type="text" placeholder="Search..." />
-      <div className="main">
-        <div className="products"></div>
-        <div className="cart"></div>
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>SocialNerds</h1>
+        <input className="search" type="text" placeholder="Search..."/>
+        <div className="main">
+          <div className="products"></div>
+          <div className="cart"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
