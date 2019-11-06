b4a31e09a7e15130d415c28a3aa194f4aca064f7
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-06 19:43:08 +0200

    Added completed render products example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
 import React from "react";
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <div className="app">
-      <h1>SocialNerds</h1>
-      <input className="search" type="text" placeholder="Search..." />
-      <div className="main">
-        <div className="products"></div>
-        <div className="cart"></div>
+export default class App extends React.Component {
+  state = {
+    products: [],
+    cartItems: []
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('https://shopping-cart-json-server.herokuapp.com/products');
+      this.setState({products: response.data});
+    } catch (e) {
+      alert('Something went wrong!');
+    }
+  }
+
+  addToCart = (product) => {
+    const {cartItems} = this.state;
+    this.setState({cartItems: [product, ...cartItems]});
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <div className="app">
+        <h1>SocialNerds</h1>
+        <input className="search" type="text" placeholder="Search..."/>
+        <div className="main">
+          <div className="products">
+            {
+              products.map(product => (
+                  <div key={product.id}>
+                    <h4>{product.name} - {product.price}</h4>
+                    <button disabled={!product.available} onClick={() => this.addToCart(product)}>Add to cart</button>
+                  </div>
+                )
+              )
+            }
+          </div>
+          <div className="cart">
+            <ul>
+
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
