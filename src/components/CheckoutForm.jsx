3eb1f93c144c41e89e73bbf2e3730cdbe84d9d2a
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-08 22:59:51 +0200

    Updated notes

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -2,35 +2,46 @@ import React from 'react';
 
 class CheckoutForm extends React.Component {
   state = {
+    email: '',
+    name: '',
+    address: ''
   };
 
   fieldsHandler = (event) => {
+    this.setState({[event.target.name]: event.target.value})
   };
 
   formHandler = (event) => {
+    event.preventDefault();
     alert(JSON.stringify(this.state));
   };
 
   render() {
+    const {name, email, address} = this.state;
     return (
-      <form>
+      <form onSubmit={this.formHandler}>
         <ul>
+          {
+            this.props.cartItems.map(item => <li key={item.id}>{item.quantity} x {item.price} - {item.name}</li>)
+          }
         </ul>
         <div>
           Total:
-
+          {
+            this.props.cartItems.reduce((sum, item) => sum + (item.price * item.quantity), 0)
+          }
         </div>
         <div>
           Name:
-          <input type="text" required/>
+          <input type="text" required  name={'name'} value={name} onChange={this.fieldsHandler}/>
         </div>
         <div>
           Email:
-          <input type="email" required/>
+          <input type="email" required name={'email'} value={email} onChange={this.fieldsHandler}/>
         </div>
         <div>
           Address:
-          <input type="text" required/>
+          <input type="text" required name={'address'} value={address} onChange={this.fieldsHandler}/>
         </div>
         <div>
           <input type="submit" value="Pay"/>
