d7eda9c0a1097f4ec19e607b2a53ea2b67747b3c
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-08 17:38:23 +0200

    Updated lists example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const movies = [
+  {
+    "title": "Star Wars: Episode I - The Phantom Menace",
+    "episode_number": "1"
+  },
+  {
+    "title": "Star Wars: Episode II - Attack of the Clones",
+    "episode_number": "2"
+  },
+  {
+    "title": "Star Wars: Episode III - Revenge of the Sith",
+    "episode_number": "3"
+  },
+  {
+    "title": "Star Wars: Episode IV - A New Hope",
+    "episode_number": "4"
+  },
+  {
+    "title": "Star Wars: Episode V - The Empire Strikes Back",
+    "episode_number": "5"
+  },
+  {
+    "title": "Star Wars: Episode VI - Return of the Jedi",
+    "episode_number": "6"
+  }
+];
+
 class List extends React.Component{
+  state = {
+    movies
+  };
+
   render() {
     return <ul></ul>;
   }
