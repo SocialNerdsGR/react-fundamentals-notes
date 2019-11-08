bbd5bfc82551c09b16d413947e131ea08534b47e
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-08 16:33:49 +0200

    Updated state example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./Counter";
 
-ReactDOM.render(<Counter />, document.querySelector("#root"));
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    const { count } = this.state;
+    this.setState({count: count + 1});
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.count}
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
