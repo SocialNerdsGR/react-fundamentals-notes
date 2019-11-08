1114542faa64dd2996194606fb4d114c5331acbb
Thanos Korakas <thanoskorakas@gmail.com>
2019-11-08 16:50:50 +0200

    Updated login form example

--- src/index.js
+++ src/index.js
@@ -2,28 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-/**
- * @hints
- * state:  const [value, setValue] = useState(initialValue);
- * input: value, onChange
- * form, input: onSubmit
- * @end
- */
-/**
- * - Handle input changes
- * - Check user credentials
- * - On failed login
- * -- Show error message
- * - On successful login
- * -- Clear inputs
- * -- Clear error
- * -- Alert user login
- */
+
 class LoginForm extends React.Component {
+  state = {
+  };
+
+  handleFieldsChange = (event) => {
+
+  };
+
+  handleSubmit = (event) => {
+
+    alert(JSON.stringify(this.state));
+  };
+
   render() {
     return (
       <form>
-        <div className="error">Incorrect username or password</div>
         <input name="email" type="email" required placeholder="Email" />
         <input name="password" type="password" required placeholder="Password" />
         <input type="submit" value="Login" />
