583351f109c4dcb52e4a8aa64dbeb507c840e000
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-02 13:31:14 +0200

    Updated dependencies and remove extra file

--- src/Hello.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class Hello extends React.Component {
-
-  render() {
-    return <span>Hello, SocialNerds</span>
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-
+  render() {
+    return <h1>Hello, SocialNerds!</h1>;
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
