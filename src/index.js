6929c64a2c2efc12a0636e0cd589cb343bfb9f28
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-02 15:31:37 +0200

    Updated example and dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Lifecycle extends React.Component {
+  componentDidMount() {
+    console.log("component did mount");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("component did update");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("should component update");
+    return true;
+  }
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+
   render() {
     return <h1>Hello world!</h1>;
   }
