bfeceda7c7738f8a258f13a7788092a474803c30
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-02 15:10:43 +0200

    Formatting and update

--- src/index.js
+++ src/index.js
@@ -3,32 +3,32 @@ import ReactDOM from "react-dom";
 
 const movies = [
   {
-    "title": "Star Wars: Episode I - The Phantom Menace",
-    "episode_number": "1"
+    title: "Star Wars: Episode I - The Phantom Menace",
+    episode_number: "1"
   },
   {
-    "title": "Star Wars: Episode II - Attack of the Clones",
-    "episode_number": "2"
+    title: "Star Wars: Episode II - Attack of the Clones",
+    episode_number: "2"
   },
   {
-    "title": "Star Wars: Episode III - Revenge of the Sith",
-    "episode_number": "3"
+    title: "Star Wars: Episode III - Revenge of the Sith",
+    episode_number: "3"
   },
   {
-    "title": "Star Wars: Episode IV - A New Hope",
-    "episode_number": "4"
+    title: "Star Wars: Episode IV - A New Hope",
+    episode_number: "4"
   },
   {
-    "title": "Star Wars: Episode V - The Empire Strikes Back",
-    "episode_number": "5"
+    title: "Star Wars: Episode V - The Empire Strikes Back",
+    episode_number: "5"
   },
   {
-    "title": "Star Wars: Episode VI - Return of the Jedi",
-    "episode_number": "6"
+    title: "Star Wars: Episode VI - Return of the Jedi",
+    episode_number: "6"
   }
 ];
 
-function Movie({title}) {
+function Movie({ title }) {
   return <li>{title}</li>;
 }
 
@@ -41,14 +41,28 @@ class List extends React.Component {
     const { movies } = this.state;
     const bestMovies = movies.filter(movie => movie.episode_number > 3);
 
-    return <React.Fragment>
-      <h1>Star wars movies</h1>
-      <ul>{movies.map(movie => <li key={movie.episode_number}>{movie.title}</li>)}</ul>
-      <h1>Separate component</h1>
-      <ul>{movies.map(movie => <Movie key={movie.episode_number} title={movie.title}/>)}</ul>
-      <h1>Best movies</h1>
-      <ul>{bestMovies.map(movie => <li key={movie.episode_number}>{movie.title}</li>)}</ul>
-    </React.Fragment>;
+    return (
+      <React.Fragment>
+        <h1>Star wars movies</h1>
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.episode_number}>{movie.title}</li>
+          ))}
+        </ul>
+        <h1>Separate component</h1>
+        <ul>
+          {movies.map(movie => (
+            <Movie key={movie.episode_number} title={movie.title} />
+          ))}
+        </ul>
+        <h1>Best movies</h1>
+        <ul>
+          {bestMovies.map(movie => (
+            <li key={movie.episode_number}>{movie.title}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
   }
 }
 
