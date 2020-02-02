27e908ca7f3b8fd82d14c145a275dc41081912b0
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-02 17:43:02 +0200

    Added hooks login former starter

--- src/index.js
+++ src/index.js
@@ -2,51 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-class LoginForm extends React.Component {
-  state = {
-    email: "",
-    password: ""
-  };
-
-  handleSubmit = event => {
+function LoginForm() {
+  function handleSubmit(event) {
     event.preventDefault();
 
-    alert(JSON.stringify(this.state));
-
-    this.setState({
-      email: "",
-      password: "",
-      error: ""
-    });
-  };
-
-  handleFields = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          name="email"
-          type="email"
-          required
-          placeholder="Email"
-          value={this.state.email}
-          onChange={this.handleFields}
-        />
-        <input
-          name="password"
-          type="password"
-          required
-          placeholder="Password"
-          value={this.state.password}
-          onChange={this.handleFields}
-        />
-        <input type="submit" value="Login" />
-      </form>
-    );
+    alert(JSON.stringify({}));
   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="email" type="email" required placeholder="Email" />
+      <input name="password" type="password" required placeholder="Password" />
+      <input type="submit" value="Login" />
+    </form>
+  );
 }
 
 ReactDOM.render(<LoginForm />, document.getElementById("root"));
