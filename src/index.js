dde5e390c513e2a9fe90b8757ff9bcfc0f876c32
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-02 17:50:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    alert(JSON.stringify({}));
+    alert(JSON.stringify({ email, password }));
+    setEmail("");
+    setPassword("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name="email" type="email" required placeholder="Email" />
-      <input name="password" type="password" required placeholder="Password" />
+      <input
+        name="email"
+        type="email"
+        required
+        placeholder="Email"
+        value={email}
+        onChange={event => {
+          setEmail(event.target.value);
+        }}
+      />
+      <input
+        name="password"
+        type="password"
+        required
+        placeholder="Password"
+        value={password}
+        onChange={event => {
+          setPassword(event.target.value);
+        }}
+      />
       <input type="submit" value="Login" />
     </form>
   );
