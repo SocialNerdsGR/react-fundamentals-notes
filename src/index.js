9f557a2fcf81883749ba1f8ce9a3ceca38e11fa1
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-03 21:06:52 +0200

    Added hooks counter completed example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => setCount(count => count + 1);
+
+  useEffect(() => {
+    window.addEventListener("click", handleIncrement);
+    return () => window.removeEventListener("click", handleIncrement);
+  }, []);
+
+  useEffect(() => {
+    document.title = `${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      {count}
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
