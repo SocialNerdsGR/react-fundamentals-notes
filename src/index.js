e9bb19daea23125b578e9082d148d31c0e601c98
Thanos Korakas <thanoskorakas@gmail.com>
2020-02-03 21:08:56 +0200

    useEffect starter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function App() {
+  return <div>Hello, Nerds!</div>;
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
